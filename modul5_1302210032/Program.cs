4b4779482d677ac92863b21e51ec1a6e3cee2fea
kamalmlzka <kamalmlzka@student.telkomuniversity.ac.id>
2023-03-21 16:44:24 +0700

    menambahkan class SimpleDataBase

--- modul5_1302210032/Program.cs
+++ modul5_1302210032/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        this.storedData = new List<T>();
+        this.inputDates = new List<DateTime>();
+    }
+
+    public void addNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void printAllData()
+    {
+        int j = 1;
+        for (int i = 0; i < this.inputDates.Count; i++)
+        {
+            Console.WriteLine("Data " + j++ + " berisi: " + storedData[i] + ", yang disimpan pada waktu UTC: " + inputDates[i]);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SimpleDataBase<int> data = new SimpleDataBase<int>();
+        data.addNewData(13);
+        data.addNewData(02);
+        data.addNewData(21);
+        data.printAllData();
+    }
+}
